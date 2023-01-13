c6eb90cfb270ce7fac2ef9a780c58244b68600e6
Daniele Coppola <dcoppola@ethz.ch>
2023-01-13 11:45:45 +0100

    decoder self attention architectures

--- models/definitions/mha_only_FF.py
+++ models/definitions/mha_only_FF.py
@@ -248,3 +248,171 @@ class FFNetwork_decoder_shrink128(nn.ModuleList):
                 o = layer(o)
             outputs.append(o * padding_mask[:, i].view((-1, 1)).repeat_interleave(self.model_dimension, dim = 1))
         return torch.stack(outputs, dim = 1)
+
+class FFNetwork_decoder_shrink256(nn.ModuleList):
+    def __init__(self, model_dimension=128,sentence_length=MAX_LEN):
+        super(FFNetwork_decoder_shrink256, self).__init__()
+        self.sentence_length=sentence_length
+        self.model_dimension=model_dimension
+        self.width=self.sentence_length*self.model_dimension
+        widths=[1,256,1]
+        self.depth=len(widths)-1
+        self.layers=nn.ModuleList()
+        for i in range(self.depth):
+            self.layers.extend([nn.LayerNorm(self.width // widths[i]),nn.Linear(self.width // widths[i], self.width // widths[i+1])])
+            if(i<self.depth-1):
+                self.layers.append(nn.LeakyReLU())
+            else:
+                self.layers[-1] = nn.Linear(self.width // widths[i], model_dimension)
+                
+    def forward(self,data,mask:torch.Tensor):
+        padding_mask = mask[:,-1]
+        mask = mask.repeat_interleave(self.model_dimension, dim = -1)
+        data = torch.reshape(data, (data.shape[0],data.shape[1]*data.shape[2]))
+        outputs = []
+        for i in range(mask.shape[1]):
+            o = data * mask[:, i, :]
+            for layer in self.layers:
+                o = layer(o)
+            outputs.append(o * padding_mask[:, i].view((-1, 1)).repeat_interleave(self.model_dimension, dim = 1))
+        return torch.stack(outputs, dim = 1)
+
+class FFNetwork_decoder_shrink128(nn.ModuleList):
+    def __init__(self, model_dimension=128,sentence_length=MAX_LEN):
+        super(FFNetwork_decoder_shrink128, self).__init__()
+        self.sentence_length=sentence_length
+        self.model_dimension=model_dimension
+        self.width=self.sentence_length*self.model_dimension
+        widths=[1,128,1]
+        self.depth=len(widths)-1
+        self.layers=nn.ModuleList()
+        for i in range(self.depth):
+            self.layers.extend([nn.LayerNorm(self.width // widths[i]),nn.Linear(self.width // widths[i], self.width // widths[i+1])])
+            if(i<self.depth-1):
+                self.layers.append(nn.LeakyReLU())
+            else:
+                self.layers[-1] = nn.Linear(self.width // widths[i], model_dimension)
+                
+    def forward(self,data,mask:torch.Tensor):
+        padding_mask = mask[:,-1]
+        mask = mask.repeat_interleave(self.model_dimension, dim = -1)
+        data = torch.reshape(data, (data.shape[0],data.shape[1]*data.shape[2]))
+        outputs = []
+        for i in range(mask.shape[1]):
+            o = data * mask[:, i, :]
+            for layer in self.layers:
+                o = layer(o)
+            outputs.append(o * padding_mask[:, i].view((-1, 1)).repeat_interleave(self.model_dimension, dim = 1))
+        return torch.stack(outputs, dim = 1)
+
+class FFNetwork_decoder_shrink8(nn.ModuleList):
+    def __init__(self, model_dimension=128,sentence_length=MAX_LEN):
+        super(FFNetwork_decoder_shrink8, self).__init__()
+        self.sentence_length=sentence_length
+        self.model_dimension=model_dimension
+        self.width=self.sentence_length*self.model_dimension
+        widths=[1,8,1]
+        self.depth=len(widths)-1
+        self.layers=nn.ModuleList()
+        for i in range(self.depth):
+            self.layers.extend([nn.LayerNorm(self.width // widths[i]),nn.Linear(self.width // widths[i], self.width // widths[i+1])])
+            if(i<self.depth-1):
+                self.layers.append(nn.LeakyReLU())
+            else:
+                self.layers[-1] = nn.Linear(self.width // widths[i], model_dimension)
+                
+    def forward(self,data,mask:torch.Tensor):
+        padding_mask = mask[:,-1]
+        mask = mask.repeat_interleave(self.model_dimension, dim = -1)
+        data = torch.reshape(data, (data.shape[0],data.shape[1]*data.shape[2]))
+        outputs = []
+        for i in range(mask.shape[1]):
+            o = data * mask[:, i, :]
+            for layer in self.layers:
+                o = layer(o)
+            outputs.append(o * padding_mask[:, i].view((-1, 1)).repeat_interleave(self.model_dimension, dim = 1))
+        return torch.stack(outputs, dim = 1)
+    
+class FFNetwork_decoder_shrink(nn.ModuleList):
+    def __init__(self, model_dimension=128,sentence_length=MAX_LEN):
+        super(FFNetwork_decoder_shrink, self).__init__()
+        self.sentence_length=sentence_length
+        self.model_dimension=model_dimension
+        self.width=self.sentence_length*self.model_dimension
+        widths=[1,2,1]
+        self.depth=len(widths)-1
+        self.layers=nn.ModuleList()
+        for i in range(self.depth):
+            self.layers.extend([nn.LayerNorm(self.width // widths[i]),nn.Linear(self.width // widths[i], self.width // widths[i+1])])
+            if(i<self.depth-1):
+                self.layers.append(nn.LeakyReLU())
+            else:
+                self.layers[-1] = nn.Linear(self.width // widths[i], model_dimension)
+                
+    def forward(self,data,mask:torch.Tensor):
+        padding_mask = mask[:,-1]
+        mask = mask.repeat_interleave(self.model_dimension, dim = -1)
+        data = torch.reshape(data, (data.shape[0],data.shape[1]*data.shape[2]))
+        outputs = []
+        for i in range(mask.shape[1]):
+            o = data * mask[:, i, :]
+            for layer in self.layers:
+                o = layer(o)
+            outputs.append(o * padding_mask[:, i].view((-1, 1)).repeat_interleave(self.model_dimension, dim = 1))
+        return torch.stack(outputs, dim = 1)
+    
+class FFNetwork_decoder_small(nn.ModuleList):
+    def __init__(self, model_dimension=128,sentence_length=MAX_LEN):
+        super(FFNetwork_decoder_small, self).__init__()
+        self.sentence_length=sentence_length
+        self.model_dimension=model_dimension
+        self.width=self.sentence_length*self.model_dimension
+        widths=[1,2,1]
+        self.depth=len(widths)-1
+        self.layers=nn.ModuleList()
+        for i in range(self.depth):
+            self.layers.extend([nn.LayerNorm(self.width * widths[i]),nn.Linear(self.width * widths[i], self.width * widths[i+1])])
+            if(i<self.depth-1):
+                self.layers.append(nn.LeakyReLU())
+            else:
+                self.layers[-1] = nn.Linear(self.width * widths[i], model_dimension)
+                
+    def forward(self,data,mask:torch.Tensor):
+        padding_mask = mask[:,-1]
+        mask = mask.repeat_interleave(self.model_dimension, dim = -1)
+        data = torch.reshape(data, (data.shape[0],data.shape[1]*data.shape[2]))
+        outputs = []
+        for i in range(mask.shape[1]):
+            o = data * mask[:, i, :]
+            for layer in self.layers:
+                o = layer(o)
+            outputs.append(o * padding_mask[:, i].view((-1, 1)).repeat_interleave(self.model_dimension, dim = 1))
+        return torch.stack(outputs, dim = 1)
+    
+class FFNetwork_decoder_mdeium(nn.ModuleList):
+    def __init__(self, model_dimension=128,sentence_length=MAX_LEN):
+        super(FFNetwork_decoder_mdeium, self).__init__()
+        self.sentence_length=sentence_length
+        self.model_dimension=model_dimension
+        self.width=self.sentence_length*self.model_dimension
+        widths=[1,2,2,1]
+        self.depth=len(widths)-1
+        self.layers=nn.ModuleList()
+        for i in range(self.depth):
+            self.layers.extend([nn.LayerNorm(self.width * widths[i]),nn.Linear(self.width * widths[i], self.width * widths[i+1])])
+            if(i<self.depth-1):
+                self.layers.append(nn.LeakyReLU())
+            else:
+                self.layers[-1] = nn.Linear(self.width * widths[i], model_dimension)
+                
+    def forward(self,data,mask:torch.Tensor):
+        padding_mask = mask[:,-1]
+        mask = mask.repeat_interleave(self.model_dimension, dim = -1)
+        data = torch.reshape(data, (data.shape[0],data.shape[1]*data.shape[2]))
+        outputs = []
+        for i in range(mask.shape[1]):
+            o = data * mask[:, i, :]
+            for layer in self.layers:
+                o = layer(o)
+            outputs.append(o * padding_mask[:, i].view((-1, 1)).repeat_interleave(self.model_dimension, dim = 1))
+        return torch.stack(outputs, dim = 1)
