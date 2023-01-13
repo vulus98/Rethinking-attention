ebe6f7ecfc7fb8f367ff2061979a77e9852c6843
Daniele Coppola <dcoppola@ethz.ch>
2023-01-13 19:13:49 +0100

    submission scripts

--- scripts/evaluate_mh_only_submit_all.sh
+++ scripts/evaluate_mh_only_submit_all.sh
@@ -4,10 +4,30 @@ if [ $# -ne 1 ]; then
     exit
 fi
 
-for i in {0..5}; do
-sbatch scripts/evaluate.sh --output=../sbatch_log/mha_only/$1/training_outputs/%j.out --substitute_model_path_d $SCRATCH/models/checkpoints/mha/$1/  --epoch_d 21 --substitute_type_d mha_only --substitute_class_d FFNetwork_$1 --layers_d $i 
-done
-sbatch scripts/evaluate.sh --output=../sbatch_log/mha_only/$1/training_outputs/%j.out --substitute_model_path_d $SCRATCH/models/checkpoints/mha/$1/  --epoch_d 21 --substitute_type_d mha_only --substitute_class_d FFNetwork_$1 
-sbatch scripts/evaluate.sh --output=../sbatch_log/mha_only/$1/training_outputs/%j.out --substitute_model_path_d $SCRATCH/models/checkpoints/mha/$1/  --epoch_d 21 --substitute_type_d mha_only --substitute_class_d FFNetwork_$1 --untrained_d
+# for i in {0..5}; do
+# sbatch  --output=../sbatch_log/mha_only/$1/evaluation_outputs/%j.out  scripts/evaluate.sh --substitute_model_path_d $SCRATCH/models/checkpoints/mha/$1/  --epoch_d 21 --substitute_type_d mha_only --substitute_class_d FFNetwork_$1 --layers_d $i 
+# done
+# sbatch  --output=../sbatch_log/mha_only/$1/evaluation_outputs/%j.out scripts/evaluate.sh --substitute_model_path_d $SCRATCH/models/checkpoints/mha/$1/  --epoch_d 21 --substitute_type_d mha_only --substitute_class_d FFNetwork_$1 
+# sbatch  --output=../sbatch_log/mha_only/$1/evaluation_outputs/%j.out  scripts/evaluate.sh --substitute_model_path_d $SCRATCH/models/checkpoints/mha/$1/  --epoch_d 21 --substitute_type_d mha_only --substitute_class_d FFNetwork_$1 --untrained_d
 
-# python3 validation_script.py --model_name transformer_128.pth --substitute_class_d FFNetwork_decoder_shrink128 --layers_d 0 --untrained_d
+# for i in {0..5}; do
+# sbatch  --output=../sbatch_log/mha_only/$1/evaluation_outputs/%j.out  scripts/evaluate.sh --substitute_model_path $SCRATCH/models/checkpoints/mha/$1/  --epoch 41 --substitute_type mha_only --substitute_class FFNetwork_$1 --layers $i 
+# done
+# sbatch  --output=../sbatch_log/mha_only/$1/evaluation_outputs/%j.out scripts/evaluate.sh --substitute_model_path $SCRATCH/models/checkpoints/mha/$1/  --epoch 41 --substitute_type mha_only --substitute_class FFNetwork_$1 
+# sbatch  --output=../sbatch_log/mha_only/$1/evaluation_outputs/%j.out  scripts/evaluate.sh --substitute_model_path $SCRATCH/models/checkpoints/mha/$1/  --epoch 41 --substitute_type mha_only --substitute_class FFNetwork_$1 --untrained
+
+# Subsittute all
+# sbatch  --output=../sbatch_log/all_substituted/%j.out scripts/evaluate.sh --substitute_model_path_d $SCRATCH/models/checkpoints/mha/decoder_shrink/  --epoch_d 41 --substitute_type_d mha_only --substitute_class_d FFNetwork_decoder_shrink  --substitute_model_path $SCRATCH/models/checkpoints/mha/shrink/  --epoch 41 --substitute_type mha_only --substitute_class FFNetwork_shrink 
+
+# Increasing substitution decoder
+layers=''
+# for i in {0..5}; do
+#         layers=$layers' '$i
+#         sbatch  --output=../sbatch_log/increasing_layers/decoder/%j.out  scripts/evaluate.sh --substitute_model_path_d $SCRATCH/models/checkpoints/mha/$1/  --epoch_d 41 --substitute_type_d mha_only --substitute_class_d FFNetwork_$1 --layers_d $layers
+# done
+# layers=''
+
+# for i in {0..5}; do
+#     layers=$layers' '$i
+#     sbatch  --output=../sbatch_log/increasing_layers/encoder/%j.out  scripts/evaluate.sh --substitute_model_path $SCRATCH/models/checkpoints/mha/$1/  --epoch 41 --substitute_type mha_only --substitute_class FFNetwork_$1 --layers $layers 
+# done
--- scripts/training_mh_FF.sh
+++ scripts/training_mh_FF.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --gpus=1
 #SBATCH --mem-per-cpu=16000
-"#SBATCH --gres=gpumem:20g"
-"#SBATCH --time 250"
-"#SBATCH --mail-user=dcoppola@ethz.ch"
-"#SBATCH --mail-type=ALL"
+#SBATCH --gres=gpumem:20g
+#SBATCH --time 250
+#SBATCH --mail-user=dcoppola@ethz.ch
+#SBATCH --mail-type=ALL
 source $HOME/miniconda3/etc/profile.d/conda.sh
 conda activate pytorch-transformer 
 python -u training_mh_FF.py "$@"
